ca38b1d0dde65f4d4889fc52a8c73aeb7ea5753f
LloydM <lloydrmontgomery@gmail.com>
2015-11-21 13:03:34 -0800

    Pushing the config file because Heroku needs it.. I'll figure out a way to solve this later.

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://admin:admin@ds053954.mongolab.com:53954/clash_tool',
+	'secret': 'supersecretclashofclanstokenencryptiondevice'
+};
+
