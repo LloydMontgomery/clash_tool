a0307206b4822e9e8a0645ebcc7b1e25ae6b8196
LloydM <lloydrmontgomery@gmail.com>
2015-12-30 14:47:49 -0800

    All routes changed to have css injectors. Back to working on profile UX

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -5,73 +5,80 @@ angular.module('app.routes', ['ngRoute'])
 	$routeProvider
 	// home page route
 	.when('/', {
-		templateUrl : 'app/views/pages/home.html'
+		templateUrl : 'app/views/pages/home.html',
+		css			: 'assets/css/home.css'
 	})
 	// login page
 	.when('/login', {
 		templateUrl : 'app/views/pages/login.html',
+		css			: 'assets/css/login.css',
 		controller  : 'mainController',
-		controllerAs: 'auth',
-		css: 'assets/css/login.css'
+		controllerAs: 'auth'
 	})
 
 	// USERS //
 	// show all users
 	.when('/users', {
 		templateUrl	: 'app/views/pages/users/all.html',
+		css			: 'assets/css/user.css',
 		controller	: 'userController',
-		controllerAs: 'user',
-		css: 'assets/css/user.css'
+		controllerAs: 'user'
 	})
 	// form to create a new user // same view as edit page 
 	.when('/users/create', {
 		templateUrl	: 'app/views/pages/users/single.html',
+		css			: 'assets/css/login.css',
 		controller	: 'userCreateController',
 		controllerAs: 'user'
 	})
 	//page to edit a user
 	.when('/users/:user_id', {
 		templateUrl	: 'app/views/pages/users/single.html',
+		css			: 'assets/css/login.css',
 		controller	: 'userEditController',
 		controllerAs: 'user'
 	})
 	//page to view a user profile
 	.when('/users/profile/:user_id', {
 		templateUrl	: 'app/views/pages/users/profile.html',
+		css			: 'assets/css/login.css',
 		controller	: 'userProfileController',
-		controllerAs: 'user',
-		css: 'assets/css/user.css'
+		controllerAs: 'user'
 	})
 
 	// WARS //
 	// show all wars
 	.when('/wars', {
 		templateUrl	: 'app/views/pages/wars/all.html',
+		css			: 'assets/css/war.css',
 		controller	: 'warListController',
 		controllerAs: 'war'
 	})
-
 	// The current war
 	.when('/wars/current', {
 		templateUrl	: 'app/views/pages/wars/single.html',
+		css			: 'assets/css/war.css',
 		controller	: 'warManipulationController',
 		controllerAs: 'war'
 	})
 	// form to create a new war // same view as edit page 
 	.when('/wars/create', {
 		templateUrl	: 'app/views/pages/wars/single.html',
+		css			: 'assets/css/war.css',
 		controller	: 'warManipulationController',
 		controllerAs: 'war'
 	})
 	//page to edit a war
 	.when('/wars/edit/:war_id', {
 		templateUrl	: 'app/views/pages/wars/single.html',
+		css			: 'assets/css/war.css',
 		controller	: 'warManipulationController',
 		controllerAs: 'war'
 	})
 	//page to view a war
 	.when('/wars/view/:war_id', {
 		templateUrl	: 'app/views/pages/wars/single.html',
+		css			: 'assets/css/war.css',
 		controller	: 'warManipulationController',
 		controllerAs: 'war'
 	});
